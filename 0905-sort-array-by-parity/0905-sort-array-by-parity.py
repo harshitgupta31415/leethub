ecf9004913abef08c01a7ed7d7d930df56aca49a
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-02-13 13:52:31 +0530

    Time: 3 ms (49.2%), Space: 20 MB (16.96%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        evenl=[]
+        oddl=[]
+        for i in nums:
+            if i%2==0:
+                evenl.append(i)
+            else:
+                oddl.append(i)
+        evenl.sort()
+        oddl.sort()
+        return evenl+oddl
