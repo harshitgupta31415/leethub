425c29fc630836fadd515280d7172a5064f59c83
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-03-11 09:23:29 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.1%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, n: int) -> int:
+        n1 = bin(n)[2:]
+        n = ""
+        for i in n1:
+            if i == "1":
+                n += "0"
+            else:
+                n += "1"
+        return int(n, 2)
