5e4a042b0163db957e0be56a8345500925d3f81e
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-03-30 20:51:32 +0530

    Time: 155 ms (41.04%), Space: 21.2 MB (24.63%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even1,even2,odd1,odd2="","","",""
+        for i in range(0,len(s1),2):
+            even1+=s1[i]
+            even2+=s2[i]
+        for i in range(1,len(s1),2):
+            odd1+=s1[i]
+            odd2+=s2[i]
+        return sorted(even1)==sorted(even2) and sorted(odd1)==sorted(odd2)
