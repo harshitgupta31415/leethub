c4f862feb0f5f705b22d4740bae9b020d4282a9b
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-04-01 21:15:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (71.75%) - LeetHub

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
@@ -1,8 +1,4 @@
 class Solution:
     def winningPlayer(self, x: int, y: int) -> str:
-        cnt = 0
-        while x > 0 and y > 3:
-            x -= 1
-            y -= 4
-            cnt += 1
-        return "Alice" if cnt % 2 == 1 else "Bob"
+        res= "Bob" if min(x,y//4)%2==0  else "Alice"
+        return res
