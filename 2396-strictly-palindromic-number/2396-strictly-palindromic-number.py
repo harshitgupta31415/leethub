402b98ee2d155b1eb5cbbca097c3050055aa0396
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-04-04 15:58:16 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.47%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+        """
         #we can use recursion but i can use np built in functio for it
+        
         import numpy as np
         for i in range(2,n-1):
             if np.base_repr(n, i)!=np.base_repr(n,i)[::-1]:
                 return False
         return True
-
-
-
-
+        """
