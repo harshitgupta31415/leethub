bb8d1009bc3b25b875462b3aabc31402a30a54df
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-04-05 17:59:18 +0530

    Time: 187 ms (10.33%), Space: 24.5 MB (82.07%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -3,12 +3,11 @@ class Solution:
         l=0
         count=0
         r=3
-        while r<len(nums)+1:
+        for j in range(len(nums) - 2):
             if nums[l]!=1:
                 count+=1
                 for i in range(l,l+3):
                     nums[i]=abs(nums[i]-1)
             l+=1
-            r+=1
         return count if 0 not in nums else -1
         
