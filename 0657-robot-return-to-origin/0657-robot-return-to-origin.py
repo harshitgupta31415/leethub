2230dfe0d213b523b9f012ba9accebfacac3b8a2
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-04-05 09:42:10 +0530

    Time: 11 ms (75.07%), Space: 19.2 MB (92.29%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,11 +1,5 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
         from collections import Counter
-        moves=dict(Counter(moves))
-        try:
-            if "U" not in moves and "D" not in moves or moves["U"]==moves["D"] :
-                if "L" not in moves and "R" not in moves or moves["L"]==moves["R"]:
-                    return True
-            return False
-        except:
-            return False
+        moves=Counter(moves)
+        return moves["U"]==moves["D"] and moves["L"]==moves["R"]
