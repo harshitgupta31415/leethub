457010014053ad976aa4c8672d1b48d20a0ff800
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-05-23 12:36:25 +0530

    Time: 0 ms (100%), Space: 19 MB (98.41%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -2,6 +2,18 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         st=nums[0]
         dip=False
+        for i in range(len(nums)):
+            if nums[i]>nums[(i+1)%len(nums)] :
+                if dip:
+                    return False
+                else:
+                    dip=True
+        return True
+
+        #same but efficient readability
+        """
+        st=nums[0]
+        dip=False
         for i in range(len(nums)-1):
             if nums[i]>nums[i+1] :
                 if dip:
@@ -14,3 +26,6 @@ class Solution:
             else:
                 return False
         return True
+
+        """
+
