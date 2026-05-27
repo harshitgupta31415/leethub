86a10d9a19d4dddea3dfb56d68d2ae866ac5d990
HARSHIT GUPTA :) <harshitgupta3.1415@gmail.com>
2026-05-27 20:24:16 +0530

    Time: 381 ms (20.59%), Space: 21.5 MB (62.94%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        small=set()
+        ans=0
+        removed=set()
+        caps=set()
+        for i in range(len(word)):
+            if word[i]!=word[i].upper():
+                if word[i].upper() not in caps:
+                    small.add(word[i])
+                else:
+                    removed.add(word[i])             
+            elif word[i]==word[i].upper():
+                caps.add(word[i])
+        print(small)
+        print(caps)
+        print(removed)
+        match=set()
+        for i in small:
+            if i.upper() in caps:
+                ans+=1
+                match.add(i)
+        for i in removed:
+            if i in match:
+                ans-=1
+        return ans
